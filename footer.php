e54725253cc7edf1d0eaca99e1963cc70203a753
Xu Wei <20105392@cqu.edu.cn>
2016-04-16 00:27:30 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -210,7 +210,7 @@ if ($current_user->is_root()) {
             tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]} 
       }); 
       </script>
-      <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+      <script type="text/javascript" src="http://acm.cqu.edu.cn/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
       </script>
   </body>
 </html>
