f428815fea5b9caffc32a52f9ebfa1520313b197
Wei Xu <20105392@cqu.edu.cn>
2016-10-08 20:35:45 +0800

    Update header.php

--- header.php
+++ header.php
@@ -74,7 +74,7 @@ include_once("functions/users.php");
                   
                   <li><a href="http://acm.cqu.edu.cn/blog">Team Blogs</a></li>
                   
-                  <li><a href="http://acm.cqu.edu.cn/blog/2016/04/22/2016年重庆大学ACM-ICPC暑假前集训计划及暑假集训队选拔方案/">Join Us</a></li>
+                  <li><a href="http://acm.cqu.edu.cn:8888/blog/2016/10/08/欢迎加入重庆大学ACM-ICPC编程俱乐部/">Join Us</a></li>
                   
              <!-- <li class="dropdown" id="more"><a class="dropdown-toggle" data-toggle="dropdown" href="teaminfo.php">More... <b class="caret"></b></a>
                 <ul class="dropdown-menu">
